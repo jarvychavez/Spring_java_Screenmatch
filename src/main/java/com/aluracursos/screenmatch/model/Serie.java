df0ae721f894b94638337001eca3dd2e26acdc99
jarvychavez <jav.chavez94@gmail.com>
2025-05-29 00:14:57 -0600

    adaptación de la clase Serie y ordenación por género

--- src/main/java/com/aluracursos/screenmatch/model/Serie.java
+++ src/main/java/com/aluracursos/screenmatch/model/Serie.java
@@ -26,12 +26,11 @@ public class Serie {
 
     @Override
     public String toString() {
-        return
-                "titulo='" + titulo + '\'' +
+        return  "genero= " + genero +
+                " titulo='" + titulo + '\'' +
                 ", totalTemporadas=" + totalTemporadas +
                 ", evaluacion=" + evaluacion +
                 ", poster='" + poster + '\'' +
-                ", genero=" + genero +
                 ", actores='" + actores + '\'' +
                 ", sinopsis='" + sinopsis + '\'';
     }
